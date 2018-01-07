4dd07a4b04145d2bb45199af05570ac358305eb4
KimBoWoon <haioer@naver.com>
2018-01-08 01:14:53 +0900

    프로젝트 rename

--- FlaskPractice/FlaskPractice/Log_Data_Blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from FlaskPractice.Log_Data_Logger import Log
-
-flaskPractice = Blueprint('flaskpractice', __name__, template_folder='../templates', static_folder='../static')
-
-Log.info('static folder : %s' % flaskPractice.static_folder)
-Log.info('template folder : %s' % flaskPractice.template_folder)
