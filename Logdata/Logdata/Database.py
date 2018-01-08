526b6dfb9573560be9311fd87b0a04e3082b5701
KimBoWoon <haioer@naver.com>
2018-01-08 16:23:31 +0900

    css 추가 & 데이터베이스 수정

--- Logdata/Logdata/Database.py
+++ Logdata/Logdata/Database.py
@@ -4,6 +4,7 @@ from flask_pymongo import PyMongo
 from pymongo import errors
 
 from Logdata.Log_Data_Logger import Log
+from Logdata.model.Logdata import Logdata
 
 
 class DBManager:
@@ -18,10 +19,23 @@ class DBManager:
     @staticmethod
     def logDataInsert(request):
         try:
-            mongo.db.logdata_android.insert({'message': request.form['message'],
-                                             'tag': request.form['tag'],
-                                             'level': request.form['level'],
-                                             'time': request.form['time']})
+            # mongo.db.logdata_android.insert({'message': request.form['message'],
+            #                                  'tag': request.form['tag'],
+            #                                  'level': request.form['level'],
+            #                                  'time': request.form['time']})
+            # mongo.db.logdata_android.insert(Logdata('hello', 'MainActivity', 'i', 1, 1, 1, 1, 1, True, 1, 1, 1))
+            mongo.db.logdata_android.insert(Logdata(request.form['message'],
+                                                    request.form['tag'],
+                                                    request.form['level'],
+                                                    request.form['time'],
+                                                    request.form['totalMemory'],
+                                                    request.form['availMemory'],
+                                                    request.form['memoryPercentage'],
+                                                    request.form['threshold'],
+                                                    request.form['lowMemory'],
+                                                    request.form['dalvikPss'],
+                                                    request.form['otherPss'],
+                                                    request.form['totalPss']))
         except pymongo.errors.DuplicateKeyError as e:
             Log.error("중복되는 키가 존재합니다.: %s" % e)
         except pymongo.errors.ServerSelectionTimeoutError as e:
--- Logdata/Logdata/model/Logdata.py
+++ Logdata/Logdata/model/Logdata.py
@@ -1,16 +1,50 @@
-from mongoengine import Document, StringField, LongField, FloatField, IntField, BooleanField
+# from mongoengine import Document, StringField, LongField, FloatField, IntField, BooleanField
+#
+#
+# class Logdata(Document):
+#     message = StringField()
+#     tag = StringField()
+#     level = StringField()
+#     time = LongField()
+#     totalMemory = LongField()
+#     availMemory = LongField()
+#     memoryPercentage = FloatField()
+#     threshold = LongField()
+#     lowMemory = BooleanField()
+#     dalvikPss = IntField()
+#     otherPss = IntField()
+#     totalPss = IntField()
+
+from flask_mongoalchemy import Document
+from mongoalchemy.fields import IntField, BoolField, FloatField, StringField
 
 
 class Logdata(Document):
+    def __init__(self, message, tag, level, time, totalMemory, availMemory, memoryPercentage, threshold, lowMemory,
+                 dalvikPss, otherPss, totalPss, **kwargs):
+        super().__init__(**kwargs)
+        self.message = message
+        self.tag = tag
+        self.level = level
+        self.time = time
+        self.totalMemory = totalMemory
+        self.availMemory = availMemory
+        self.memoryPercentage = memoryPercentage
+        self.threshold = threshold
+        self.lowMemory = lowMemory
+        self.dalvikPss = dalvikPss
+        self.otherPss = otherPss
+        self.totalPss = totalPss
+
     message = StringField()
     tag = StringField()
     level = StringField()
-    time = LongField()
-    totalMemory = LongField()
-    availMemory = LongField()
+    time = IntField()
+    totalMemory = IntField()
+    availMemory = IntField()
     memoryPercentage = FloatField()
-    threshold = LongField()
-    lowMemory = BooleanField()
+    threshold = IntField()
+    lowMemory = BoolField()
     dalvikPss = IntField()
     otherPss = IntField()
     totalPss = IntField()
