05b08c3a8a2ad6913e17b171e905c077f81b7582
KimBoWoon <haioer@naver.com>
2018-01-09 17:45:11 +0900

    file update

--- Logdata/Log_Data_Blueprint.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-from Logdata.Log_Data_Logger import Log
-
-logdata = Blueprint('flaskpractice', __name__, template_folder='../templates', static_folder='../static')
-
-Log.info('static folder : %s' % logdata.static_folder)
-Log.info('template folder : %s' % logdata.template_folder)
--- runserver.py
+++ runserver.py
@@ -1,6 +1,14 @@
-from Logdata import create_app
+"""
+This script runs the FlaskWebProject1 application using a development server.
+"""
 
-app = create_app()
+from os import environ
+from MirisHuesWebapp import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8080)
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
