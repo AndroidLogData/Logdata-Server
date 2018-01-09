1e96a70ff733c1f25eb4310e208ae9eaf5146415
KimBoWoon <haioer@naver.com>
2018-01-09 21:43:48 +0900

    post로 변경

--- Logdata/Logdata/controller/LogDataController.py
+++ Logdata/Logdata/controller/LogDataController.py
@@ -11,14 +11,14 @@ def index():
     return render_template('index.html')
 
 
-@logdata.route('/logdata', methods=['GET', 'PUT'])
+@logdata.route('/logdata', methods=['GET', 'POST'])
 def logData():
     if request.method == 'GET':
         data = DBManager.getLogdata()
         return render_template('logdata_view.html', logData=data)
-    elif request.method == 'PUT':
+    elif request.method == 'POST':
         print(request.headers)
-        jsonString = request.get_json()
-        print(jsonString)
-        DBManager.logDataInsert(jsonString)
+        # jsonString = request.get_json()
+        # print(jsonString)
+        DBManager.logDataInsert(request)
         return 'success'
