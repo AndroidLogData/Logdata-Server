e49631df9ec635a2026b473388c1b7bdf4d2d279
KimBoWoon <haioer@naver.com>
2018-01-18 00:13:25 +0900

    chash chart 변경

--- Logdata/Logdata/controller/CrashController.py
+++ Logdata/Logdata/controller/CrashController.py
@@ -49,19 +49,22 @@ def crash():
             'Build': temp.build
         }
 
-        labels = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
-        month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        values = []
+        crashCount = []
+        items = CrashData.objects().all()
+        timeSet = set()
 
-        for x in range(12):
-            start = '2018-' + str(x + 1) + '-' + '1 0:00:00'
-            end = '2018-' + str(x + 1) + '-' + str(month[x]) + ' 23:59:59'
-            start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-            end = datetime.datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
-            # values.append(DBManager.getCrashDataProjection(start, end).count())
-            values.append(CrashData.objects(time__lte=end, time__gte=start).all().count())
+        for item in items:
+            timeSet.add(str(item.time))
 
-        return render_template('crashdata_view.html', crashdata=data, values=values, labels=labels)
+        timeSet = sorted(timeSet)
+        print(timeSet)
+
+        for item in timeSet:
+            crashCount.append(CrashData.objects(time=item).all().count())
+
+        print(crashCount)
+
+        return render_template('crashdata_view.html', crashdata=data, values=zip(timeSet, crashCount))
     elif request.method == 'POST':
         jsonString = request.get_json()
         item = CrashData(datetime.datetime.strptime(jsonString['Time'], '%Y-%m-%d %H:%M:%S'),
